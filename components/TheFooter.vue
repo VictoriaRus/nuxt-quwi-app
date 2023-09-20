34202c095d2288b2f7518f83f5091167e10375dd
Вика <ruseckaya@cmit.ru>
2023-09-20 16:17:30 +0300

    init project

---
+++ components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+<div>footer</div>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TheHeader />
+    <Nuxt />
+    <TheFooter />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
