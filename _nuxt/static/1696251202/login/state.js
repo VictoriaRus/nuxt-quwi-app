d99329a2b5ae12368e13c3203d1201bfeb0bfa3a
Вика <ruseckaya@cmit.ru>
2023-10-02 15:59:34 +0300

    0b1eae5

---
+++ _nuxt/static/1696251202/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-quwi-app\u002F_nuxt\u002Fstatic\u002F1696251202",layout:"empty",error:a,state:{authenticated:false,error:{},tasks:{tasks:[]}},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002Fnuxt-quwi-app\u002F",assetsPath:"\u002Fnuxt-quwi-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1696251202/tasks/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-quwi-app\u002F_nuxt\u002Fstatic\u002F1696251202",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Ftasks",config:{_app:{basePath:"\u002Fnuxt-quwi-app\u002F",assetsPath:"\u002Fnuxt-quwi-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
